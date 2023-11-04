96b95688d1fc4fbd88efafe651d37c76c1381b22
Nicolas Wender Fontes Queiroz <nicolas.w3nder@gmail.com>
2023-11-04 12:37:26 -0300

    criando um model de Produtos e configurando o basico do Django Administration

--- app/Api/models.py
+++ app/Api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField(max_length=800, default="Descrição do produto")
+    preco = models.DecimalField(max_digits=4, decimal_places=2)
+    quantidade = models.PositiveIntegerField()
+    "fazer a parada de escolher em uma seleção"
+    CATEGORIAS = [
+        ("Eletrônicos", "Eletrônicos"),
+        ("Casa e Decoração", "Casa e Decoração"),
+        ("Moda", "Moda"),
+    ]
+    categoria = models.CharField(max_length=16, choices=CATEGORIAS)
+
+    def __str__(self):
+        return f"{self.nome} {self.descricao} {self.preco} {self.quantidade} {self.categoria}"
